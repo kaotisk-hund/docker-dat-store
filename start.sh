dd4a0343f26e0f158b73d1910dd25aa2034a35d0
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 09:00:35 +0200

    Detaching when run, restart always

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --name dat-store -v {$PWD}/storage/dat:/dat -v {$PWD}/storage/dat-config:/dat-config dat-store:latest
+docker run --name dat-store -d --restart always -v {$PWD}/storage/dat:/dat -v {$PWD}/storage/dat-config:/dat-config dat-store:latest
